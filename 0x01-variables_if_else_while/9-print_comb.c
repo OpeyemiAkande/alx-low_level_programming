2359a2a5d9f4b3df9a7f742630081bc008f11b28
Opeyemi Akande <akande212@gmail.com>
2022-03-13 16:25:02 +0000

    Crazy changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,16 @@ int main(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		for (num2 = '0'; num <= '9'; num++)
+		putchar(num);
+		if (num < '9')
 		{
-			putchar(num);
-			putchar(num2);
+			putchar(',');
+			putchar(' ');
 		}
-		putchar(',');
-		putchar(' ');
+		else
+		{
+			putchar('$');
+		}
 	}
 	return (0);
 }
