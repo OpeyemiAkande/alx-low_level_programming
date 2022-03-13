316eaa95c301459cc7e017c5789cc6043c30add9
Opeyemi Akande <akande212@gmail.com>
2022-03-13 06:37:14 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	int ch;
+	for(ch= 'a'; ch <= 'z'; ch++)
+	{	putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
