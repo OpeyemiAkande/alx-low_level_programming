139a820093b7fd415f91c44f6c84357776a9c57c
Opeyemi Akande <akande212@gmail.com>
2022-03-16 21:42:32 +0000

    Code for printing the minutes per day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - Prints the minutes of a day 
+ */
+void jack_bauer(void)
+{
+	int hr, min = 60;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		while (min < 60)
+		{
+			printf("%2d:%2d", hr, min);
+			min++;
+		}
+		min = 0;
+	}
+}
