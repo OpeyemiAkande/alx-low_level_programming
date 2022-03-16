ed865b68bc53f3dc427b6d318822b5c7d9469782
Opeyemi Akande <akande212@gmail.com>
2022-03-16 07:57:46 +0000

    creation of the function to print alphabets 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef PRINT_ALPHABET
 #define PRINT_ALPHABET
+#ifndef PRINT_ALPHABET_X10
+#define PRINT_ALPHABET_X10
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /* PRINT_ALPHABET */
