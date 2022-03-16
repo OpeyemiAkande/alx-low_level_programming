e59a4a06ac757df79fe97cb8b1d58071c1e206c7
Opeyemi Akande <akande212@gmail.com>
2022-03-16 04:16:29 +0000

    Corrected the error in compilation for the 1-alphabet program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,21 +5,13 @@
  *Return: 0 (Success)
  */
 
-char print_alphabet(void)
+int main(void)
 {
-	char ch;
-
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
+		putchar('\n');
 	}
-	putchar('\n');
-}
 
-int main(void)
-{
-	char r[27] = {print_alphabet()};
-
-	printf("%s", r);
 	return (0);
 }
