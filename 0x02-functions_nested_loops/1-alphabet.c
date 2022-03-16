a2f917ddcd8adc73ae1ad0c21c84fafbd5697a68
Opeyemi Akande <akande212@gmail.com>
2022-03-16 06:03:24 +0000

    some changes on task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,18 @@
  *Return: 0 (Success)
  */
 
+void print_alphabet (void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
+
+
 int main(void)
 {
 	print_alphabet();
