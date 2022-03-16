95adb92aab44be24f2e5a0e51dee9b281822df16
Opeyemi Akande <akande212@gmail.com>
2022-03-16 22:46:37 +0000

    Code for the times table function

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - Prints the times table from 0 to 9
+ */
+void times_table(void)
+{
+	int r, i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = (i * j);
+			printf("%3d", r);
+		}
+	}
+}
