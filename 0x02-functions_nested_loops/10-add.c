34d0875b6fd69a0de5e53abd0d27a836e9a4ab18
Opeyemi Akande <akande212@gmail.com>
2022-03-17 00:21:21 +0000

    Code for adding numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  *add - adds two numbers togehther
- *Return: The sum of the added number 
+ *Return: The sum of the added number
  *@i: a parameter to be used
  *@j: a parameter to be used
  */
