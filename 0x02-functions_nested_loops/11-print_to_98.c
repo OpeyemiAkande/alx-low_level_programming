843660b5ebc47f5aac1b22e28fa13638374a2d3b
Opeyemi Akande <akande212@gmail.com>
2022-03-17 04:51:37 +0000

    renamed the code file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *add - adds two numbers togehther
- *Return: The sum of the added number
- *@i: a parameter to be used
- *@j: a parameter to be used
+ *print_to_98 - adds two numbers togehther
+ *@n: a parameter to be used
  */
 void print_to_98(int n)
 {
-	for (; n < 98; n++)
+	if (n < 98)
+	{	
+		for (; n < 98; n++)
+		{
+			printf("%d,", n);
+		}
+	}
+	else if (n > 98)
 	{
-		printf("%d,", n);
+		for (; n >= 98; n--)
+		{
+			printf("%d,", n);
+		}
 	}
 	printf("%d\n", n);
 }
