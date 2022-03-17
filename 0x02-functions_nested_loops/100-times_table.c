6501eaa1ea763dae437b12b1cc1f66a0c2e9ce1a
Opeyemi Akande <akande212@gmail.com>
2022-03-17 05:17:49 +0000

    100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - Prints the times table from 0 to 9
+ */
+void prints_times_table(int n)
+{
+	int r, i, j, n;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			r = (i * j);
+			printf("%3d,", r);
+		}
+		printf("\n");
+	}
+}
