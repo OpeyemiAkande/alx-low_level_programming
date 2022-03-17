a96eb68b0cabc662039fb315c94202c212ff12fb
Opeyemi Akande <akande212@gmail.com>
2022-03-17 00:17:59 +0000

    Code for adding numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * times_table - Prints the times table from 0 to 9
+ *add - adds two numbers togehther
+ *Return: The sum of the added number 
  */
 int add(int i, int j)
 {
 	int num_sum;
 
 	num_sum = i + j;
-	return num_sum;
+	return (num_sum);
 }
