0ae5abd4038dc8c84970112d449018bd3485f467
Opeyemi Akande <akande212@gmail.com>
2022-03-18 13:47:31 +0000

    Code to print a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line using underscore
+ * @n: the number of times to print underscores
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,10 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-/** main - check the code.
+/**
+ * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-	more_numbers();
-	return (0);
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
 }
