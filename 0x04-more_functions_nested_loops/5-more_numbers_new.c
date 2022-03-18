8430fd8b4c0e2317be962809d7cff8e6730471c0
Opeyemi Akande <akande212@gmail.com>
2022-03-18 12:35:53 +0000

    Made some changes

--- 0x04-more_functions_nested_loops/5-more_numbers_new.c
+++ 0x04-more_functions_nested_loops/5-more_numbers_new.c
@@ -6,13 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i, first_number, second_number, result;
+	int i; 
+	int first_number;
+	int second_number; 
+	int result;
 
 	i = 0;
 	result = 0;
 	while (i < 10)
 	{
-		if (result <= 14)
+		while (result <= 14)
 		{
 			if (result < 10)
 			{
