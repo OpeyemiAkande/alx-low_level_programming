8dfacd4d1d1013c2d43e1eb7fb3b99f2b1479ae2
Opeyemi Akande <akande212@gmail.com>
2022-03-18 14:38:51 +0000

    Code to print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints a diagonal line
+ * @n: the number of times to print
+ */
+void print_diagonal(int n)
+{
+	int i, b;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (b = 0; b <= i; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
