0c8f2c2ce491e274afcaf7b2f116d1a6562c0f75
Opeyemi Akande <akande212@gmail.com>
2022-03-18 02:04:56 +0000

    Print numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - multiplies two integers
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if ((num != 2) && (num != 4))
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
+	
