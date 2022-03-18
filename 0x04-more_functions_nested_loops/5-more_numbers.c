a7b775cdcad2100150bd3d46055d5adbb19f5aeb
Opeyemi Akande <akande212@gmail.com>
2022-03-18 02:30:30 +0000

    Print numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - multiplies two integers
+ */
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (num = '0'; num <= '14'; num++)
+		{
+			_putchar(num);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int mul(int a, int b);
 int _putchar(int c);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 #endif /* RETURNS VALUES FOR UPPERCASE */
