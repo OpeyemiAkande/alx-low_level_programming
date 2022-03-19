a1db988bda203c90101830c349ac043e9e43bc74
Opeyemi Akande <akande212@gmail.com>
2022-03-19 04:19:51 +0000

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infiite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 * Infinite loop no increase of variable
+	 * putchar(i);
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\O/\n");
+	return (0);
+}
