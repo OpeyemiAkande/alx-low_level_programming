8d7567fd6bf7bd450523832afd0a7d644ddf8823
Opeyemi Akande <akande212@gmail.com>
2022-03-22 04:43:25 +0000

    my commit for task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - a function that converts a string into an integer
+ *
+ * @s: the pointer the string that would be converted into an integer
+ *
+ * Return: This function returns 0 as a possible value
+ */
+char _atoi(char *s)
+{
+	unsigned int number, i;
+	int sign;
+
+	sign = 1;
+	number = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_numerical(s[i]))
+		{
+			number = (s[i] - 48) + number * 10;
+			if (s[i] + 1 == ' ')
+				break;
+		}
+		else if (s[i] == '-')
+		{
+			sign += -1
+		}
+	}
+	return (number + sign);
+}
