7722b88a3f9995b0e432545b49f1a66a2bb7d228
Opeyemi Akande <akande212@gmail.com>
2022-03-22 02:18:30 +0000

    my commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,19 +8,18 @@
  */
 void print_rev(char *s)
 {
-	int count, i, j;
+	int  i;
 
 	i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
-		count = i;
 	}
 
-	for ((i - 1); i >= 0; i--)
+	for (i = (i - 1); i >= 0; i--)
 	{
-		_putchar(s[j]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
