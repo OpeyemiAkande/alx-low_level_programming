6f87ad8e0c26917faaed1d0287c83316897b624f
Opeyemi Akande <akande212@gmail.com>
2022-03-22 04:21:02 +0000

    Code for task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * _strcopy - copies the string contained by str
+ * _strcpy - copies the string contained by str
  *
  * @dest: the pointer the char that would be changed
  * @src: pointer to a char that will be changed
  *
  * Return: This function returns nothing
  */
-void *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
