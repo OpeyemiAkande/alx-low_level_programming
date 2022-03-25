4d2158e04c6a2ad2595b25b6d5088a1866e3fc1a
Opeyemi Akande <akande212@gmail.com>
2022-03-25 15:11:35 +0000

    made some changes

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,18 +15,18 @@ void reverse_array(int *a, int n)
 	start_c = a;
 	end_c = a;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
 		end_c++;
 	}
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < (n / 2); i++)
 	{
 		c = *end_c;
 		*end_c = *start_c;
 		*start_c = c;
 
-		*start_c++;
-		*end_c--;
+		start_c++;
+		end_c--;
 	}
 }
