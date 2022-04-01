9037262d0dfa6c99960e95530026bada250ed524
Opeyemi Akande <akande212@gmail.com>
2022-04-01 14:41:21 +0000

    a new commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
- * print_my_name - prints the name of the program
- */
-void print_my_name(char** argv)
-{
-	int i;
-	
-	for (i = 0; argv[0][i] == '\0'; i++)
-	{
-		_putchar(argv[0][i]);
-	}
-}
-
-/**
  * main - entry point
  * Return: 0 (success)
  */
 int main(int argc, char **argv)
 {
-	print_my_name(argv);
+	int i;
+
+	for (i = argc - argc; argv[0][i] != '\0'; i++)
+	{
+		_putchar(argv[0][i]);
+	}
+	_putchar('\n');
 	return (0);
 }
