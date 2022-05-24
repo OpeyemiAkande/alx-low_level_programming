dd3357b75d4f607474d00864be2752aad8d96e7e
Opeyemi Akande <akande212@gmail.com>
2022-05-24 11:48:33 +0000

    corrections for print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -20,7 +20,7 @@ void print_number(int n)
 	{
 		m = n;
 	}
-	
+
 	d = m;
 	count = 1;
 
@@ -30,6 +30,6 @@ void print_number(int n)
 		count *= 10;
 	}
 
-	for (; count >= 1; count /=10)
+	for (; count >= 1; count /= 10)
 		_putchar(((m / count) % 10) + 48);
 }
